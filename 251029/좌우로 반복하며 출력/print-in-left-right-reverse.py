d2c777790e97f9b86840bf271c0ba3ab757bc0f6
dkfjslrks19 <dkfjslrks19@gmail.com>
2025-10-29 01:06:12 +0900

    Title: 좌우로 반복하며 출력, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251029/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+
+for i in range(1,n+1):
+    row = []
+    cnt=1
+    for j in range(n):
+        row.append(cnt)
+        cnt+=1
+    if i%2==0:
+        row.reverse()
+    for j in range(n):
+        print(row[j],end='')
+    print()
+
+
+
+

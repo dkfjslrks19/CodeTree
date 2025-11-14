76db88943cba591b137dd268c854f3cc07fb9356
dkfjslrks19 <dkfjslrks19@gmail.com>
2025-11-15 08:35:24 +0900

    Title: 코로나 메뉴얼, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251115/코로나 메뉴얼/covid-manual.py
@@ -0,0 +1,17 @@
+a = list(input().split())
+b = list(input().split())
+c = list(input().split())
+
+num = 0
+
+
+if a[0]=='Y' and int(a[1])>=37:
+    num += 1
+if a[0]=='Y' and int(a[1])>=37:
+    num += 1
+if a[0]=='Y' and int(a[1])>=37:
+    num += 1
+if num>=2:
+    print('E')
+else:
+    print('N')    

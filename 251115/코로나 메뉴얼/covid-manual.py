a6e1992d5c206d64a7b1217a7313a4074d42eddc
dkfjslrks19 <dkfjslrks19@gmail.com>
2025-11-15 08:36:34 +0900

    Title: 코로나 메뉴얼, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 251115/코로나 메뉴얼/covid-manual.py
+++ 251115/코로나 메뉴얼/covid-manual.py
@@ -7,9 +7,9 @@ num = 0
 
 if a[0]=='Y' and int(a[1])>=37:
     num += 1
-if a[0]=='Y' and int(a[1])>=37:
+if b[0]=='Y' and int(b[1])>=37:
     num += 1
-if a[0]=='Y' and int(a[1])>=37:
+if c[0]=='Y' and int(c[1])>=37:
     num += 1
 if num>=2:
     print('E')

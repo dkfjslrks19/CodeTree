85e955a72384097dbbbf74ba570285a8562dbcbb
dkfjslrks19 <dkfjslrks19@gmail.com>
2025-11-28 10:23:11 +0900

    Title: 별표 출력하기 6, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251128/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(n):
+    print('  '*(i) + '* '*(2*(n-i)-1), end='')
+    print()
+
+for i in range(n-1,0,-1):
+    print('  '*(i-1) + '* '*(2*(n-i)+1), end='')
+    print()
